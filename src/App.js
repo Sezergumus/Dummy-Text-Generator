63ae4a54af92e721b6acfb8b0795c72d8f55199e
Sezer <sezer2004@gmail.com>
2021-03-11 10:46:53 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
+import data from "./data"
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0){
+      amount = 1;
+    }
+    setText(data.slice(0, amount))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <h3>Dummy Text Generator</h3>
+      <form className="dummy-form" onSubmit={handleSubmit}>
+        <label style={{fontSize: "1.25rem"}} htmlFor="amount">Parapgraphs:</label>
+        <input 
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="dummy-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </div>
   );
 }
